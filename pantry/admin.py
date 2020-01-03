f6c9bbc107a1a8634242eda050a71216193b750a
Zarpas <imartin1974@gmail.com>
2020-01-03 18:25:29 +0100

    Pantry application, models defined and admin registered

--- pantry/admin.py
+++ pantry/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ubicacion
+from .models import Estante
+from .models import Referencia
+from .models import Alimento
+
 
 # Register your models here.
+
+admin.site.register(Ubicacion)
+admin.site.register(Estante)
+admin.site.register(Referencia)
+admin.site.register(Alimento)
--- pantry/models.py
+++ pantry/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Ubicacion(models.Model):
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    creado = models.DateField(auto_now_add=True)
+    enUso = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Estante(models.Model):
+    refUbicacion = models.ForeignKey('Ubicacion', on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    descripcion = models.TextField()
+    creado = models.DateField(auto_now_add=True)
+    enUso = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Referencia(models.Model):
+    nombre = models.CharField(max_length=50)
+    fabricante = models.CharField(max_length=50)
+    codigoBarras = models.CharField(max_length=20)
+    unidad = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Alimento(models.Model):
+    refEstante = models.ForeignKey('Estante', on_delete=models.CASCADE)
+    refReferencia = models.ForeignKey('Referencia', on_delete=models.CASCADE)
+    cantidad = models.DecimalField(max_digits=5, decimal_places=2)
+    caducidad = models.DateField()
+
+    def __str__(self):
+        return self.cantidad
